ad2de2f4afd115a161810bf9a63f1d3c9402986b
Francis Addae <franco09e@yahoo.com>
2024-08-30 02:19:03 -0400

    making final tweaks

--- extract_load_runner.py
+++ extract_load_runner.py
@@ -5,23 +5,23 @@ from extract_load import load_data_into_warehouse
 from creds import AUTH_KEY, SHEET_NAME, TABLE_NAME
 
 
-# @task(name="FoodAgricStats_DBT")
-# def trigger_dbt_flow(initaiter=False) -> str:
-#     if initaiter:
-#         DbtCoreOperation(
-#             commands=["dbt run -s +food_and_agricultural_stats --full-refresh -t dev"],
-#             project_dir="transform",
-#             profiles_dir="~/.dbt",
-#         ).run()
-#         return "yes"
-#     else:
-#         return "no"
+@task(name="companyCAM_DBT")
+def trigger_dbt_flow(initaiter=False) -> str:
+    if initaiter:
+        DbtCoreOperation(
+            commands=["dbt run -s commerce+ --full-refresh -t dev"],
+            project_dir="/Users/Francis/Desktop/CODE/companyCam/transform",
+            profiles_dir="~/.dbt",
+        ).run()
+        return "yes"
+    else:
+        return "no"
 
 @flow(name="companyCamELT")
 def companyCamELTFlow():
     e = extract_data_from_source(AUTH_KEY, SHEET_NAME)
     l = load_data_into_warehouse(e, TABLE_NAME)
-    # trigger_dbt_flow(l)
+    trigger_dbt_flow(l)
 
 
 if __name__ == '__main__':
